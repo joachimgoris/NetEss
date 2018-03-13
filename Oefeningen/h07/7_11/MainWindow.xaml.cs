3c7fc4b96e014da9e33403a4d8c18f3103faeb73
Joachim Goris <gorisjoachim@hotmail.com>
2018-03-13 18:19:15 +0100

    Don't know anymore

---
+++ Oefeningen/h07/7_11/MainWindow.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Windows;
+
+namespace _7_11
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public static Random Rdm1 = new Random(5000);
+        public static  Random Rdm2 = new Random(5000);
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void RandomButton1_Click(object sender, RoutedEventArgs e)
+        {
+            RandomLabel1.Content = Convert.ToDouble(Rdm1.Next());
+        }
+
+        private void RandomButton2_Click(object sender, RoutedEventArgs e)
+        {
+            RandomLabel2.Content = Convert.ToDouble(Rdm2.Next());
+        }
+    }
+}
---
+++ Oefeningen/h08/8_11/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _8_11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Geef het aantal visjes");
+            int aantalVisjes = Convert.ToInt16(Console.ReadLine());
+            for (int teller = aantalVisjes; teller>= 0; teller--)
+            {
+                Console.WriteLine(teller+" kleine visjes, die zwommen naar de zee.");
+                Console.WriteLine("Nee, zei de moeder, ik ga niet mee.");
+                Console.WriteLine("Ik blijf liever in die vieze vuile ouwe sloot.");
+                Console.WriteLine("Want in de zee daar zwemt de haai en die bijt je dan... dood.");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
