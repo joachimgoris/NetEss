1ec8e1f7666180eb0718d2e9325337bc7c6e80e9
Joachim Goris <gorisjoachim@hotmail.com>
2018-03-21 21:48:08 +0100

    oefening wekker

---
+++ Oefeningen/h11/11_7/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _11_7
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        void SetTijdLabel(string input)
+        {
+            this.tijdLabel.Content = input;
+        }
+
+        string getTijdTextbox()
+        {
+            return this.tijdTextbox.Text;
+        }
+
+        private void alarmButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Oefeningen/h11/11_7/Wekker.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Threading;
+
+namespace _11_7
+{
+    internal abstract class Wekker
+    {
+        protected DateTime TijdNu;
+        private const int AlarmDuur = 10;
+        protected DispatcherTimer timer;
+
+
+        protected int GetAlarmDuur()
+        {
+            return AlarmDuur;
+        }
+
+        
+    }
+}
