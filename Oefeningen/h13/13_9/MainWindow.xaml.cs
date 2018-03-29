b304169383bd49ec8bbb934e80292cd2a73d1142
Joachim Goris <gorisjoachim@hotmail.com>
2018-03-29 09:07:16 +0200

    13_9

---
+++ Oefeningen/h13/13_9/MainWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace _13_9
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            colorComboBox.SelectedIndex = 0;
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            ComboBoxItem item = (ComboBoxItem) colorComboBox.SelectedItem;
+            string selectedColor = Convert.ToString(item.Content);
+
+            colorLabel.Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString(selectedColor));
+            colorLabel.Background = (Brush) new BrushConverter().ConvertFromString(selectedColor);
+        }
+
+    }
+}
